bb05dfbd83bf1a085ea61b1b58f34be7d82e62d5
erick <a22006182@alunos.ulht.pt>
2023-08-14 19:48:13 +0100

    lista e BottomBar

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/infrastructure/theme';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SafeArea } from './src/components/utility/safe-area.component';
 
 import {useFonts as useOswald, Oswald_400Regular,} from '@expo-google-fonts/oswald';
 
 import {useFonts as useLato, Lato_400Regular,} from '@expo-google-fonts/lato';
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () =>(
+	<Text>Settings</Text>
+)
+const Map = () => (
+	<Text>Map</Text>
+)
 
 export default function App() {
 
@@ -24,7 +37,13 @@ export default function App() {
   return (
 	<>
 		<ThemeProvider theme={theme}>
-			<RestaurantsScreen />
+			<NavigationContainer>
+				<Tab.Navigator>
+					<Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+					<Tab.Screen name="Map" component={Map} />
+					<Tab.Screen name="Settings" component={Settings} />
+				</Tab.Navigator>
+			</NavigationContainer>
 		</ThemeProvider>
 	</>
   );
