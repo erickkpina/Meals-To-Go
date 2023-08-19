05a0a0af29c515cf2bec98d5ed7c428a335b5a0b
erick <a22006182@alunos.ulht.pt>
2023-08-19 16:58:53 +0100

    Search funcionando

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -5,43 +5,46 @@ import { locationRequest, locationTransform } from "./location.service";
 export const LocationContext = React.createContext();
 
 export const LocationContextProvider = ({ children }) => {
-  const [keyword, setKeyword] = useState("San Francisco");
-  const [location, setLocation] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+    const [keyword, setKeyword] = useState("San Francisco");
+    const [location, setLocation] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-  const onSearch = (searchKeyword) => {
-    setIsLoading(true);
-    setKeyword(searchKeyword);
-    if (!searchKeyword.length) {
-      // don't do anything
-      return;
-    }
-    locationRequest(searchKeyword.toLowerCase())
-      .then(locationTransform)
-      .then((result) => {
-        setIsLoading(false);
-        setLocation(result);
-        console.log(result);
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        setError(err);
-        console.log(err);
-      });
-  };
+    const onSearch = (searchKeyword) => {
+        setIsLoading(true);
+        setKeyword(searchKeyword);
+    };
+
+    useEffect(() => {
+    if (!keyword.length) {
+        return;
+        }
+        locationRequest(keyword.toLowerCase())
+        .then(locationTransform)
+        .then((result) => {
+            setIsLoading(false);
+            setLocation(result);
+            console.log(result);
+        })
+        .catch((err) => {
+            setIsLoading(false);
+            setError(err);
+            console.log(err);
+        });
+    }, [keyword]);
+  
 
   return (
-    <LocationContext.Provider
-      value={{
-        isLoading,
-        error,
-        location,
-        search: onSearch,
-        keyword,
-      }}
-    >
-      {children}
-    </LocationContext.Provider>
-  );
+        <LocationContext.Provider
+            value={{
+                isLoading,
+                error,
+                location,
+                search: onSearch,
+                keyword,
+            }}
+        >
+            {children}
+        </LocationContext.Provider>
+    );
 };
--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,6 +1,7 @@
-import React, { useState, createContext, useEffect, useMemo } from "react";
+import React, { useState, useContext, createContext, useEffect, useMemo } from "react";
 
 import { restaurantsRequest, restaurantsTransform } from "./restaurants.service";
+import { LocationContext } from "../location/location.context";
 
 export const RestaurantsContext = createContext();
 
@@ -8,11 +9,13 @@ export const RestaurantsContextProvider = ({ children }) => {
 	const [restaurants, setRestaurants] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
+	const { location } = useContext(LocationContext);
 
-	const retriveRestaurants = () => {
+	const retriveRestaurants = (loc) => {
 		setIsLoading(true);
+		setRestaurants([]);
 		setTimeout(() => {
-			restaurantsRequest()
+			restaurantsRequest(loc)
 			.then(restaurantsTransform)
 			.then((restaurants) => {
 				setIsLoading(false);
@@ -24,8 +27,11 @@ export const RestaurantsContextProvider = ({ children }) => {
 		}, 2000)
 	};
 	useEffect(() => {
-		retriveRestaurants();
-	} , []);
+		if(location){
+			const locationString = `${location.lat},${location.lng}`;
+			retriveRestaurants(locationString);
+		}
+	} , [location]);
 
     return (
 		<RestaurantsContext.Provider
