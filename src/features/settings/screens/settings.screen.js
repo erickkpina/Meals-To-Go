78654da1041263f35f26cbd8f872ce1dcda5d1b8
erick <a22006182@alunos.ulht.pt>
2023-09-05 13:09:13 +0100

    camera feature

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useCallback, useState } from "react";
 import styled from "styled-components/native";
 
 import { List, Avatar } from "react-native-paper";
@@ -7,25 +7,53 @@ import { Spacer } from "../../../components/spacer/spacer.component";
 import { Text } from "../../../components/typography/text.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+import { TouchableOpacity } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const SettingsItem = styled(List.Item)`
     padding: ${(props) => props.theme.space[3]};
 `;
 
-const AVatarContainer = styled.View`
+const AvatarContainer = styled.View`
     align-items: center;
 `;
 
 export const SettingsScreen = ({ navigation }) => {
     const { onLogout, user } = useContext(AuthenticationContext);
+    const [photo, setPhoto] = useState(null);
+
+    const getProfilePicture = async (currentUser) => {
+      const photoUri = await AsyncStorage.getItem(`${currentUser.uid}-photo`);
+      setPhoto(photoUri);
+    };
+  
+    useFocusEffect(
+        useCallback(() => {
+        getProfilePicture(user);
+        }, [user])
+    );
+
     return (
         <SafeArea>
-            <AVatarContainer>
-                <Avatar.Icon size={180} icon="human" backgroundColor="purple" />
+            <AvatarContainer>
+            <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+                {!photo && (
+                    <Avatar.Icon size={180} icon="camera" backgroundColor="#2182BD" />
+                )}
+                {photo && (
+                    <Avatar.Image
+                    size={180}
+                    source={{ uri: photo }}
+                    backgroundColor="#2182BD"
+                    />
+                )}
+                </TouchableOpacity>
                 <Spacer position="top" size="large">
                     <Text variant="label">{user.email}</Text>
                 </Spacer>
-            </AVatarContainer>
+        </AvatarContainer>
             
             <List.Section>
                 <SettingsItem
--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 
 import { SettingsScreen } from "../../features/settings/screens/settings.screen";
 import { FavouritesScreen } from "../../features/settings/screens/favourites.screen";
+import { CameraScreen } from "../../features/settings/screens/camera.screen";
 
 import {
   createStackNavigator,
@@ -11,22 +12,23 @@ import {
 const SettingsStack = createStackNavigator();
 
 export const SettingsNavigator = ({ route, navigation }) => {
-  return (
-    <SettingsStack.Navigator
-      
-      screenOptions={{
-        headerMode:"screen",
-        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-      }}
-    >
-      <SettingsStack.Screen
-        options={{
-          header: () => null,
-        }}
-        name="SettingsStack"
-        component={SettingsScreen}
-      />
-      <SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
-    </SettingsStack.Navigator>
-  );
+	return (
+		<SettingsStack.Navigator
+			
+			screenOptions={{
+				headerMode:"screen",
+				cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+			}}
+			>
+			<SettingsStack.Screen
+				options={{
+				header: () => null,
+				}}
+				name="SettingsStack"
+				component={SettingsScreen}
+			/>
+			<SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
+			<SettingsStack.Screen name="Camera" component={CameraScreen} />
+		</SettingsStack.Navigator>
+	);
 };
